82c9eb2c98cf141cf89b6cd298381115a84186dc
Valerie Camejo <valerie260492@gmail.com>
2017-03-09 15:00:29 -0400

    Update footer-mobile.blade.php

--- resources/views/includes/footer-mobile.blade.php
+++ resources/views/includes/footer-mobile.blade.php
@@ -2,19 +2,19 @@
   <div class="container">
     <div class="col-sm-2">
       <ul class="social-icons">
-        @if(isset(Auth::user()->id_tipousuario) && Auth::user()->id_tipousuario==3 && Auth::user()->active==1)
+        @if(isset(Auth::user()->user_type_id) && Auth::user()->user_type_id==3 && Auth::user()::STATUS_ACTIVE)
           <li>
-            {{ HTML::link('usuario/terminos-y-servicios','Términos y servicios'); }}
+            {!! Html::link('usuario/terminos-y-servicios','Términos y servicios') !!}
           </li>
           <li>
-            {{ HTML::link('usuario/politicas-de-privacidad','Políticas de privacidad'); }}
+            {!! Html::link('usuario/politicas-de-privacidad','Políticas de privacidad') !!}
           </li>
         @else
           <li>
-            {{ HTML::link('terminos-y-servicios','Términos y servicios'); }}
+            {!! Html::link('terminos-y-servicios','Términos y servicios') !!}
           </li>
           <li>
-            {{ HTML::link('politicas-de-privacidad','Políticas de privacidad'); }}
+            {!! Html::link('politicas-de-privacidad','Políticas de privacidad') !!}
           </li>
         @endif
       </ul>
@@ -25,22 +25,22 @@
         <li class="Siguenos">Síguenos en nuestras redes:</li>
         <li>
           <a href="https://www.facebook.com/fantasycobra/?fref=ts">
-            {{ HTML::image('images/fb2.png') }}
+            {!! Html::image('images/fb2.png') !!}
           </a>
         </li>
         <li>
           <a href="https://twitter.com/Fantasy_Cobra">
-            {{ HTML::image('images/tw2.png') }}
+            {!! Html::image('images/tw2.png') !!}
           </a>
         </li>
         <li>
           <a href="https://www.instagram.com/fantasycobra">
-            {{ HTML::image('images/insta2.png') }}
+            {!! Html::image('images/insta2.png') !!}
           </a>
         </li>
         <li>
           <a href="skype:contacto_11689?call">
-              {{ HTML::image('images/skype-3.png') }}
+              {!! Html::image('images/skype-3.png') !!}
           </a>
         </li>
       </ul>
