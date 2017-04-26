a839ec63e6f91b662cc4e0d25f3d415c9afb6424
svasquezwd <aurivasquez2009@gmail.com>
2017-04-26 14:08:25 -0400

    add template create team

--- app/Http/Controllers/TeamUserController.php
+++ app/Http/Controllers/TeamUserController.php
@@ -61,7 +61,6 @@ class TeamUserController extends Controller {
 
   public function players() {
     $players = Player::players($_GET['championship'],$_GET['type_play'],$_GET['date_team']);
-
     echo json_encode($players);
   }
 
