9a063dca7614292f505bb05bd8a8b567674979dd
Valerie Camejo <valerie260492@gmail.com>
2017-08-14 17:19:15 -0400

    Fixes for validations an view Register

--- resources/views/users/competitions.blade.php
+++ resources/views/users/competitions.blade.php
@@ -1,15 +1,18 @@
 @extends ('layouts.template')
 
 @section ('content')
-<div id="page-content-wrapper">
-    <!--  /Content-wrapper -->
-    <h3 class="Titulo1">MIS COMPETICIONES</h3>
+<!-- Abre Wrapper -->
+<div id="wrapper">
+  <div id="page-content-wrapper">
+      <!--  /Content-wrapper -->
+      <h3 class="Titulo1">MIS COMPETICIONES</h3>
+
+  <!-- -------------------------------- MODALES -------------------------------- -->
+  @include('modal/competition')
+  @include('includes.my-competitions')
+  @include('includes.my-competitions-mobile')
+  @include('includes/footer-mobile')
+  </div>
+      <!--  /Content-wrapper -->
 </div>
-<!-- -------------------------------- MODALES -------------------------------- -->
-@include('modal/competition')
-@include('includes.my-competitions')
-@include('includes.my-competitions-mobile')
-@include('includes/footer-mobile')
-</div>
-<!--  /Content-wrapper -->
 @stop
