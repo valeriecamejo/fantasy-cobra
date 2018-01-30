c425011caa332be904d04d30a0c2204c2ba4b8ab
Rafael Figueredo <rafael.figueredo@condoragency.com>
2018-01-30 15:46:50 -0400

    modify Seeder DatabaseSeeder uncomment lines

--- database/seeds/Team_SubscribersTableSeeder.php
+++ database/seeds/Team_SubscribersTableSeeder.php
@@ -16,13 +16,11 @@ class Team_SubscribersTableSeeder extends Seeder
   {
     $faker = Faker::create();
     $datenow = Carbon::now()->toDateTimeString();
-    $competition = DB::table('competitions')>where('date', '>',$datenow)->inRandomOrder()
-                ->first()
 
     DB::table('team_subscribers')->insert(array(
                                                 array(
                                                       'sport_id'       => 1,
-                                                      'competition_id' => $competition->id,
+                                                      'competition_id' => DB::table('competitions')->where('date', '>',$datenow)->inRandomOrder()->value('id'),
                                                       'team_id'        => 1,
                                                       'team_user_id'   => 1,
                                                       'amount'         => 5000,
@@ -33,10 +31,12 @@ class Team_SubscribersTableSeeder extends Seeder
                                                       'bonus'          => 1000,
                                                       'balance'        => 11000,
                                                       'is_winner'      => true,
+                                                      'created_at'     => Carbon::now()->toDateTimeString(),
+                                                      'updated_at'     => Carbon::now()->toDateTimeString()
                                                       ),
                                                 array(
                                                       'sport_id'       => 2,
-                                                      'competition_id' => $competition->id,
+                                                      'competition_id' => DB::table('competitions')->where('date', '>',$datenow)->inRandomOrder()->value('id'),
                                                       'team_id'        => 2,
                                                       'team_user_id'   => 1,
                                                       'amount'         => 2000,
@@ -46,11 +46,13 @@ class Team_SubscribersTableSeeder extends Seeder
                                                       'balance_after'  => 10000,
                                                       'bonus'          => 1000,
                                                       'balance'        => 9500,
-                                                      'is_winner'      => true,
+                                                      'is_winner'      => false,
+                                                      'created_at'     => Carbon::now()->toDateTimeString(),
+                                                      'updated_at'     => Carbon::now()->toDateTimeString()
                                                       ),
                                                 array(
                                                       'sport_id'       => 1,
-                                                      'competition_id' => $competition->id,
+                                                      'competition_id' => DB::table('competitions')->where('date', '>',$datenow)->inRandomOrder()->value('id'),
                                                       'team_id'        => 1,
                                                       'team_user_id'   => 1,
                                                       'amount'         => 1000,
@@ -61,6 +63,8 @@ class Team_SubscribersTableSeeder extends Seeder
                                                       'bonus'          => 500,
                                                       'balance'        => 7500,
                                                       'is_winner'      => true,
+                                                      'created_at'     => Carbon::now()->toDateTimeString(),
+                                                      'updated_at'     => Carbon::now()->toDateTimeString()
                                                       ),
                                                 ));
   }
